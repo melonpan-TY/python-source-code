480e3827b14c1d806d0e825f85fd9fb22a75192a
melonpan-TY <77038877+melonpan-TY@users.noreply.github.com>
2021-01-06 16:07:27 +0900

    Create Changes_in_cells.py

---
+++ Changes_in_cells.py
@@ -0,0 +1,11 @@
+#python ver.3.8.0
+#pip ver 20.3.3
+
+#セルの変更
+
+import openpyxl #openpyxlをインポートする
+wb = openpyxl.load_workbook("test.xlsx") #作業フォルダからエクセルファイルを指定
+wa = wb.active #アクティブのシートを指定
+wa.cell(row = 1 ,column = 2).value = "こんばんは" #セルのrow:行 column:列を指定する
+wb.save("test.xlsx") #セーブする
+wb.close() #ファイル閉じる
