8ef3fcaa4799f6f414749edb84306396b175109e
melonpan-TY <77038877+melonpan-TY@users.noreply.github.com>
2021-01-06 16:02:27 +0900

    Update Change_sheet_name.py

--- Change_sheet_name.py
+++ Change_sheet_name.py
@@ -1,5 +1,7 @@
-#シート名の変更
+#python ver.3.8.0
+#pip ver 20.3.3
 
+#シート名の変更
 import openpyxl #openpyxlをインポートする
 wb = openpyxl.load_workbook("test.xlsx") #作業フォルダからエクセルファイルを指定
 ws = wb.worksheets[0] #シートインデックス0（シート番号は０始まり）
